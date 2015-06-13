bc8a7131ac2417085445f8497f783ca44ce7f7d2
Gordon Hollingworth <gordon@fiveninjas.com>
2015-06-13 13:26:46 +0000

    Add dt-blob.bin support for Raspberry Pi targets

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -33,7 +33,10 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 make_target() {
-  : # nothing to make
+  if [ -f $PROJECT_DIR/$PROJECT/bootloader/dt-blob.dts ]; then
+    echo Compiling device tree blob
+    dtc -O dtb -o dt-blob.bin $PROJECT_DIR/$PROJECT/bootloader/dt-blob.dts
+  fi
 }
 
 makeinstall_target() {
@@ -42,6 +45,7 @@ makeinstall_target() {
     cp -PRv bootcode.bin $INSTALL/usr/share/bootloader
     cp -PRv fixup_x.dat $INSTALL/usr/share/bootloader/fixup.dat
     cp -PRv start_x.elf $INSTALL/usr/share/bootloader/start.elf
+    cp -PRv dt-blob.bin $INSTALL/usr/share/bootloader/dt-blob.bin
 
     cp -PRv $PKG_DIR/scripts/update.sh $INSTALL/usr/share/bootloader
     cp -PRv $PKG_DIR/files/3rdparty/bootloader/config.txt $INSTALL/usr/share/bootloader
