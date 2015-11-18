ac65b2869800e0706f7c5465038282fc991d2de5
MikeBuzz <MikeBuzz@users.noreply.github.com>
2015-11-18 13:32:32 +0000

    correct path the the led patterns

--- projects/Slice/filesystem/usr/config/shutdown.sh
+++ projects/Slice/filesystem/usr/config/shutdown.sh
@@ -3,14 +3,14 @@
 case "$1" in
   halt)
     hdparm -Y /dev/sda
-    led_png /storage/.config/shutdown.png
+    led_png /usr/share/kodi/media/ledpatterns/shutdown.png
     ;;
   poweroff)
     hdparm -Y /dev/sda
-    led_png /storage/.config/shutdown.png
+    led_png /usr/share/kodi/media/ledpatterns/shutdown.png
     ;;
   reboot)
-    led_png /storage/.config/shutdown.png
+    led_png /usr/share/kodi/media/ledpatterns/shutdown.png
     ;;
   *)
     ;;
